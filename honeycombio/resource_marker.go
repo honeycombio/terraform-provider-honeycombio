c6b11f3b03e60cd5691837fcaecdd8a0105dc3f6
Koenraad Verheyden <github@kvrhdn.dev>
2020-07-29 00:56:38 +0200

    Update go-honeycombio to v0.0.4

--- honeycombio/resource_marker.go
+++ honeycombio/resource_marker.go
@@ -35,12 +35,12 @@ func newMarker() *schema.Resource {
 func resourceMarkerCreate(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*honeycombio.Client)
 
-	data := honeycombio.CreateMarkerData{
+	data := honeycombio.MarkerCreateData{
 		Message: d.Get("message").(string),
 		Type:    d.Get("type").(string),
 		URL:     d.Get("url").(string),
 	}
-	marker, err := client.CreateMarker(data)
+	marker, err := client.Markers.Create(data)
 	if err != nil {
 		return err
 	}
@@ -52,30 +52,19 @@ func resourceMarkerCreate(d *schema.ResourceData, meta interface{}) error {
 func resourceMarkerRead(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*honeycombio.Client)
 
-	markers, err := client.ListMarkers()
+	marker, err := client.Markers.Get(d.Id())
 	if err != nil {
+		if err == honeycombio.ErrNotFound {
+			d.SetId("")
+			return nil
+		}
 		return err
-	}
-
-	marker := findMarkerWithID(markers, d.Id())
-	if marker == nil {
-		d.SetId("")
-		return nil
 	}
 
 	d.SetId(marker.ID)
 	d.Set("message", marker.Message)
 	d.Set("type", marker.Type)
 	d.Set("url", marker.URL)
-	return nil
-}
-
-func findMarkerWithID(markers []honeycombio.Marker, id string) *honeycombio.Marker {
-	for _, m := range markers {
-		if m.ID == id {
-			return &m
-		}
-	}
 	return nil
 }
 
