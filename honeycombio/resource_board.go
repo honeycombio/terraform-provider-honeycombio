3661e2a70cfa0f35c3060e582bb02c2a1d1e4e58
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-08-03 23:42:19 -0400

    Very WIP PR for board resource

---
+++ honeycombio/resource_board.go
@@ -0,0 +1,150 @@
+package honeycombio
+
+import (
+    "encoding/json"
+
+    "github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+    honeycombio "github.com/kvrhdn/go-honeycombio"
+)
+
+func newBoard() *schema.Resource {
+    return &schema.Resource{
+        Create: resourceBoardCreate,
+        Read:   resourceBoardRead,
+        Update: resourceBoardUpdate,
+        Delete: resourceBoardDelete,
+
+        Schema: map[string]*schema.Schema{
+            "name": {
+                Type:     schema.TypeString,
+                Required: true,
+            },
+            "description": {
+                Type:     schema.TypeString,
+                Optional: true,
+            },
+            "style": {
+                Type:     schema.TypeString,
+                Optional: true,
+                Default:  "list",
+            },
+            "query": {
+                Type: schema.TypeList,
+                Elem: &schema.Resource{
+                    Schema: map[string]*schema.Schema{
+                        "caption": {
+                            Type:     schema.TypeString,
+                            Optional: true,
+                        },
+                        "dataset": {
+                            Type:     schema.TypeString,
+                            Required: true,
+                        },
+                        "query_json": {
+                            Type:     schema.TypeString,
+                            Required: true,
+                        },
+                    },
+                },
+                Optional: true,
+            },
+        },
+    }
+}
+
+func resourceBoardCreate(d *schema.ResourceData, meta interface{}) error {
+    client := meta.(*honeycombio.Client)
+
+    b, err := expandBoard(d)
+    if err != nil {
+        return err
+    }
+
+    b, err = client.Boards.Create(b)
+    if err != nil {
+        return err
+    }
+
+    d.SetId(b.ID)
+    return resourceTriggerRead(d, meta)
+}
+
+func resourceBoardRead(d *schema.ResourceData, meta interface{}) error {
+    client := meta.(*honeycombio.Client)
+
+    b, err := client.Boards.Get(d.Id())
+    if err != nil {
+        if err == honeycombio.ErrNotFound {
+            d.SetId("")
+            return nil
+        }
+        return err
+    }
+
+    d.SetId(b.ID)
+    d.Set("name", b.Name)
+    d.Set("description", b.Description)
+    d.Set("style", b.Style)
+
+    //TODO queries
+    //d.Set("queries", queries)
+
+    return nil
+}
+
+func resourceBoardUpdate(d *schema.ResourceData, meta interface{}) error {
+    client := meta.(*honeycombio.Client)
+
+    dataset := d.Get("dataset").(string)
+    t, err := expandTrigger(d)
+    if err != nil {
+        return err
+    }
+
+    t, err = client.Triggers.Update(dataset, t)
+    if err != nil {
+        return err
+    }
+
+    d.SetId(t.ID)
+    return resourceTriggerRead(d, meta)
+}
+
+func resourceBoardDelete(d *schema.ResourceData, meta interface{}) error {
+    client := meta.(*honeycombio.Client)
+
+    dataset := d.Get("dataset").(string)
+
+    return client.Triggers.Delete(dataset, d.Id())
+}
+
+func expandBoard(d *schema.ResourceData) (*honeycombio.Board, error) {
+    var queries []honeycombio.BoardQuery
+
+    qs := d.Get("query").([]interface{})
+    for _, q := range qs {
+        m := q.(map[string]interface{})
+        caption := m["caption"].(string)
+        dataset := m["dataset"].(string)
+        queryJson := m["query_json"].(string)
+        var query honeycombio.QuerySpec
+        err := json.Unmarshal([]byte(queryJson), &query)
+        if err != nil {
+            return nil, err
+        }
+        queries = append(queries, honeycombio.BoardQuery{
+            Caption: caption,
+            Dataset: dataset,
+            Query:   query,
+        })
+    }
+
+    board := &honeycombio.Board{
+        ID:          d.Id(),
+        Name:        d.Get("name").(string),
+        Description: d.Get("description").(string),
+        Style:       honeycombio.BoardStyle( d.Get("style").(string)),
+        Queries:     queries,
+    }
+    return board, nil
+}
---
+++ honeycombio/resource_board_test.go
@@ -0,0 +1,131 @@
+package honeycombio
+
+import (
+    "errors"
+    "fmt"
+    "testing"
+
+    "github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+    "github.com/hashicorp/terraform-plugin-sdk/terraform"
+    honeycombio "github.com/kvrhdn/go-honeycombio"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestAccHoneycombioBoard_basic(t *testing.T) {
+    dataset := testAccDataset()
+
+    resource.Test(t, resource.TestCase{
+        PreCheck:  func() { testAccPreCheck(t) },
+        Providers: testAccProviders,
+        Steps: []resource.TestStep{
+            {
+                Config: testAccBoardConfig(dataset),
+                Check: resource.ComposeTestCheckFunc(
+                    testAccCheckBoardExists(t, "honeycombio_board.test"),
+                ),
+            },
+        },
+    })
+}
+
+func testAccBoardConfig(dataset string) string {
+    return fmt.Sprintf(`
+data "honeycombio_query" "test" {
+    count = 2
+    calculation {
+        op     = "AVG"
+        column = "duration_ms"
+    }
+    filter {
+        op     = ">"
+        column = "duration_ms"
+        value  = count.index 
+    }
+}
+
+resource "honeycombio_board" "test" {
+    name  = "Test board from terraform-provider-honeycombio"
+    style = "list"
+     
+    query {
+        caption = "test query 0"
+        dataset = "%s"
+        query_json = data.honeycombio_query.test[0].json
+    }
+      
+    query {
+        caption = "test query 1"
+        dataset = "%s"
+        query_json = data.honeycombio_query.test[1].json
+    }
+
+}`, dataset, dataset)
+}
+
+func testAccCheckBoardExists(t *testing.T, name string) resource.TestCheckFunc {
+    return func(s *terraform.State) error {
+        resourceState, ok := s.RootModule().Resources[name]
+        if !ok {
+            return fmt.Errorf("not found: %s", name)
+        }
+
+        client := testAccProvider.Meta().(*honeycombio.Client)
+        createdBoard, err := client.Boards.Get(resourceState.Primary.ID)
+        if err != nil {
+            return fmt.Errorf("could not find created trigger: %w", err)
+        }
+
+        expectedBoard := &honeycombio.Board{
+            ID:          createdBoard.ID,
+            Name:        "Test board from terraform-provider-honeycombio",
+            Description: "",
+            Style:       honeycombio.BoardStyleList,
+            Queries: []honeycombio.BoardQuery{
+                {
+                    Caption: "test query 0",
+                    Dataset: testAccDataset(),
+                    Query: honeycombio.QuerySpec{
+                        Calculations: []honeycombio.CalculationSpec{
+                            {
+                                Op:     honeycombio.CalculateOpAvg,
+                                Column: &[]string{"duration_ms"}[0],
+                            },
+                        },
+                        Filters: []honeycombio.FilterSpec{
+                            {
+                                Column: "avg",
+                                Op:     "duration_ms",
+                                Value:  0,
+                            },
+                        },
+                    },
+                },
+                {
+                    Caption: "test query 1",
+                    Dataset: testAccDataset(),
+                    Query: honeycombio.QuerySpec{
+                        Calculations: []honeycombio.CalculationSpec{
+                            {
+                                Op:     honeycombio.CalculateOpAvg,
+                                Column: &[]string{"duration_ms"}[0],
+                            },
+                        },
+                        Filters: []honeycombio.FilterSpec{
+                            {
+                                Column: "avg",
+                                Op:     "duration_ms",
+                                Value:  1,
+                            },
+                        },
+                    },
+                },
+            },
+        }
+
+        ok = assert.Equal(t, expectedBoard, createdBoard)
+        if !ok {
+            return errors.New("created board did not match expected board")
+        }
+        return nil
+    }
+}
