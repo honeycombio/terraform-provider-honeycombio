a387bad21ad50cb19baa67bffbda091222c325c8
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-08-06 00:26:02 -0400

    Don't allow a value when filter op in ('exists', 'does-not-exist')

--- honeycombio/data_source_query_test.go
+++ honeycombio/data_source_query_test.go
@@ -1,6 +1,7 @@
 package honeycombio
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -56,8 +57,7 @@ const expectedJSON string = `{
   "filters": [
     {
       "column": "trace.parent_id",
-      "op": "does-not-exist",
-      "value": ""
+      "op": "does-not-exist"
     },
     {
       "column": "app.tenant",
@@ -67,3 +67,29 @@ const expectedJSON string = `{
   ],
   "filter_combination": "AND"
 }`
+
+//Honeycomb API blows up if you send a Value with a FilterOp of `exists` or `does-not-exist`
+func TestAccDataSourceHoneycombioQuery_noValueForExists(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config:      testBadExistsQuery(),
+				ExpectError: regexp.MustCompile("Filter operation exists must not"),
+			},
+		},
+	})
+}
+
+func testBadExistsQuery() string {
+	return `
+data "honeycombio_query" "test" {
+    filter {
+        column = "column"
+        op     = "exists"
+        value  = "this-value-should-not-be-here"
+    }
+}
+`
+}
