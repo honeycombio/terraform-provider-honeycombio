431d865a2e05ca597d0c50052626e4123507681b
Andrew Fitzgerald <andrewcfitzgerald@gmail.com>
2020-08-06 00:21:00 -0400

    Add board example

---
+++ example/board/main.tf
@@ -0,0 +1,51 @@
+provider "honeycombio" {
+}
+
+variable "dataset" {
+  type = string
+}
+
+locals {
+  percentiles = ["P50", "P75", "P90", "P95"]
+}
+
+data "honeycombio_query" "query" {
+  count = length(local.percentiles)
+  calculation {
+    op     = local.percentiles[count.index]
+    column = "duration_ms"
+  }
+
+  filter {
+    column = "trace.parent_id"
+    op     = "does-not-exist"
+  }
+
+  filter {
+    column = "app.tenant"
+    op     = "="
+    value  = "ThatSpecialTenant"
+  }
+
+  filter_combination = "AND"
+
+  # also supported: breakdowns
+
+}
+
+resource "honeycombio_board" "board" {
+  name        = "Request percentiles"
+  description = "${join(", ", local.percentiles)} of all requests for ThatSpecialTenant for the last 15 minutes."
+  style       = "list"
+
+
+  //Use dynamic config blocks to generate a query for each of the percentiles we're interested in
+  dynamic "query" {
+    for_each = local.percentiles
+    content {
+      caption    = query.value
+      dataset    = var.dataset
+      query_json = data.honeycombio_query.query[query.key].json
+    }
+  }
+}
