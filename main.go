5945de4c759990f89e04b0c6c987bf08e2819ff6
Luke Carrier <luke@carrier.im>
2022-03-02 14:47:02 +0000

    Allow provider debugging

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/honeycombio/terraform-provider-honeycombio/honeycombio"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return honeycombio.Provider()
-		},
-	})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: honeycombio.Provider}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "registry.terraform.io/honeycombio/honeycombio", opts)
+		if err != nil {
+			log.Fatalf(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
