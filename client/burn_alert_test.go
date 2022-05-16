c0532ed934ff07df5185d0e0262ce35b52695d8f
Jason Harley <jason@honeycomb.io>
2022-05-16 17:01:40 -0400

    client: first pass at slo and burnalert support

---
+++ client/burn_alert_test.go
@@ -0,0 +1,97 @@
+package client
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBurnAlerts(t *testing.T) {
+	ctx := context.Background()
+
+	var burnAlert *BurnAlert
+	var err error
+
+	c := newTestClient(t)
+	dataset := testDataset(t)
+
+	sli, err := c.DerivedColumns.Create(ctx, dataset, &DerivedColumn{
+		Alias:      "sli.ba_slo_test",
+		Expression: "LT($duration_ms, 1000)",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	slo, err := c.SLOs.Create(ctx, dataset, &SLO{
+		Name:             "BurnAlert Test SLO",
+		TimePeriodDays:   7,
+		TargetPerMillion: 999000,
+		SLI:              SLIRef{Alias: sli.Alias},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// remove SLO and SLI at the end of the test run
+	t.Cleanup(func() {
+		c.SLOs.Delete(ctx, dataset, slo.ID)
+		c.DerivedColumns.Delete(ctx, dataset, sli.ID)
+	})
+
+	t.Run("Create", func(t *testing.T) {
+		data := &BurnAlert{
+			ExhaustionMinutes: int(24 * time.Hour),
+			SLO:               SLORef{ID: slo.ID},
+		}
+
+		burnAlert, err = c.BurnAlerts.Create(ctx, dataset, data)
+
+		assert.NoError(t, err, "failed to create BurnAlert")
+		assert.NotNil(t, burnAlert.ID, "BurnAlert ID is empty")
+		assert.NotNil(t, burnAlert.CreatedAt, "created at is empty")
+		assert.NotNil(t, burnAlert.UpdatedAt, "updated at is empty")
+		// copy dynamic fields before asserting equality
+		data.ID = burnAlert.ID
+		data.CreatedAt = burnAlert.CreatedAt
+		data.UpdatedAt = burnAlert.UpdatedAt
+		assert.Equal(t, data, burnAlert)
+	})
+
+	t.Run("ListForSLO", func(t *testing.T) {
+		results, err := c.BurnAlerts.ListForSLO(ctx, dataset, slo.ID)
+
+		assert.NoError(t, err, "failed to list burn alerts for SLO")
+		assert.Contains(t, results, *burnAlert, "newly created BurnAlert not in list of SLO's burn alerts")
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		getBA, err := c.BurnAlerts.Get(ctx, dataset, burnAlert.ID)
+
+		assert.NoError(t, err, "failed to get BurnAlert by ID")
+		assert.Equal(t, burnAlert, getBA)
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		burnAlert.ExhaustionMinutes = int(4 * time.Hour)
+
+		result, err := c.BurnAlerts.Update(ctx, dataset, burnAlert)
+
+		assert.NoError(t, err, "failed to update BurnAlert")
+		// copy dynamic field before asserting equality
+		burnAlert.UpdatedAt = result.UpdatedAt
+		assert.Equal(t, burnAlert, result)
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		err = c.BurnAlerts.Delete(ctx, dataset, burnAlert.ID)
+
+		assert.NoError(t, err, "failed to delete BurnAlert")
+	})
+
+	t.Run("Get_NotFound", func(t *testing.T) {
+		_, err := c.BurnAlerts.Get(ctx, dataset, burnAlert.ID)
+
+		assert.Equal(t, ErrNotFound, err)
+	})
+}
