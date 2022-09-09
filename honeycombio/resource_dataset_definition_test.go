9716a5d8b5987d56f814a874e5e20b173d619879
Michael Simo <michaelsimo@honeycomb.io>
2022-09-09 09:17:23 -0700

    add remaining tests, try a few different update validations

--- honeycombio/resource_dataset_definition_test.go
+++ honeycombio/resource_dataset_definition_test.go
@@ -55,6 +55,7 @@ func TestAccHoneycombioDatasetDefinition_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.name", "duration_ms"),
 					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.value", "duration_ms"),
 				),
+				ExpectNonEmptyPlan: true,
 			},
 			{
 				// update the name field
@@ -175,6 +176,7 @@ func TestAccHoneycombioDatasetDefinition_basic(t *testing.T) {
 					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.name", "duration_ms"),
 					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.value", "gt50_duration_ms"),
 				),
+				ExpectNonEmptyPlan: true,
 			},
 			{ // delete the name field
 				Config: `
@@ -209,7 +211,50 @@ func TestAccHoneycombioDatasetDefinition_basic(t *testing.T) {
 
 				// TODO: find name field, what would HCL return
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.name.name", "null"),
+				//resource.TestCheckTypeSetElemAttr("honeycombio_dataset_definition.test", "field.0.name", ""),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+			{
+				// reset to default
+				Config: `
+				resource "honeycombio_dataset_definition" "test" {
+				dataset    = "testacc"
+				
+				field {
+					name = "duration_ms"
+					value = "duration_ms"
+				}
+
+				field {
+					name = "name"
+					value = "name"
+				}
+
+				field {
+					name = "parent_id"
+					value = "trace.parent_id"
+				}
+
+				field {
+					name = "service_name"
+					value = "service_name"
+				}
+
+				field {
+					name = "trace_id"
+					value = "trace.trace_id"
+				}
+
+				field {
+					name = "span_id"
+					value = "trace.span_id"
+				}
+					}`,
+
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.name", "duration_ms"),
+					resource.TestCheckResourceAttr("honeycombio_dataset_definition.test", "field.0.value", "duration_ms"),
 				),
 			},
 		},
