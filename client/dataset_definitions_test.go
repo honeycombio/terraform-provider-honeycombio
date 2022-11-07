85466aa7179d4fffc45e41c72aecaa22e5fe4bea
Josslyn Zhang <josslynzhang@honeycomb.io>
2022-11-07 11:10:00 -0700

    update client side test

--- client/dataset_definitions_test.go
+++ client/dataset_definitions_test.go
@@ -11,53 +11,96 @@ import (
 func TestDatasetDefinitions(t *testing.T) {
 	ctx := context.Background()
 
-	// Definition to create
-	//  "trace_id": { "name": "trace.trace_id" }, "duration_ms": { "name": "request_processing_time"}
-	// Exisiting Derived Column: GTE(INT($duration_ms), 50)
-	//
-
-	traceIDDefinition := DefinitionColumn{
-		Name:       "trace.trace_id",
-		ColumnType: "column",
-	}
-
-	// durationMsDefinition := DefinitionColumn{
-	// 	Name:       "GTE($duration_ms, 50)",
-	// 	ColumnType: "derived_column",
-	// }
-
-	// Create the parent that contains all of the dataset defs (TraceID = trace_id)
-	datasetDefinition := DatasetDefinition{
-		TraceID: traceIDDefinition,
-		// DurationMs: durationMsDefinition,
-	}
+	var datasetDef *DatasetDefinition
+	var err error
 
 	c := newTestClient(t)
 	dataset := testDataset(t)
+
+	dcs, err := c.DerivedColumns.Create(ctx, dataset, &DerivedColumn{
+		Alias:      "test123",
+		Expression: "GTE(INT($duration_ms), 50)",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// remove dataset definition DC at end of test run
+	t.Cleanup(func() {
+		c.DerivedColumns.Delete(ctx, dataset, dcs.ID)
+	})
+
+	t.Run("Create", func(t *testing.T) {
+		// set the Trace ID definition
+		traceIDDefinition := DefinitionColumn{
+			Name:       "trace.trace_id",
+			ColumnType: "column",
+		}
+
+		// set the durationMs definition
+		durationMsDerivedDefinition := DefinitionColumn{
+			Name:       dcs.Alias,
+			ColumnType: "derived_column",
+		}
+
+		data := &DatasetDefinition{
+			TraceID:    traceIDDefinition,
+			DurationMs: durationMsDerivedDefinition,
+		}
+
+		datasetDef, err := c.DatasetDefinitions.Create(ctx, dataset, data)
+		assert.NoError(t, err)
+		assert.Equal(t, "column", datasetDef.TraceID.ColumnType)
+		assert.Equal(t, "derived_column", datasetDef.DurationMs.ColumnType)
+	})
 
 	t.Run("Get", func(t *testing.T) {
 		result, err := c.DatasetDefinitions.Get(ctx, dataset)
 		assert.NoError(t, err)
 		assert.Equal(t, "column", result.TraceID.ColumnType)
-		assert.Equal(t, "column", result.DurationMs.ColumnType)
+		assert.Equal(t, "derived_column", result.DurationMs.ColumnType)
 	})
 
-	// set the Trace ID definition
 	t.Run("Update", func(t *testing.T) {
-		result, err := c.DatasetDefinitions.Update(ctx, dataset, &datasetDefinition)
+
+		data := &Column{
+			KeyName:     "error_test1",
+			Hidden:      BoolPtr(false),
+			Description: "This column is created by a test",
+			Type:        ColumnTypePtr(ColumnTypeFloat),
+		}
+
+		_, err = c.Columns.Create(ctx, dataset, data)
+
+		errorDefinition := DefinitionColumn{
+			Name:       data.KeyName,
+			ColumnType: "column",
+		}
+
+		// durationMsDefinition := DefinitionColumn{
+		// 	Name:       "",
+		// 	ColumnType: "",
+		// }
+
+		datasetDef = &DatasetDefinition{
+			Error: errorDefinition,
+			// DurationMs: durationMsDefinition,
+		}
+
+		result, err := c.DatasetDefinitions.Update(ctx, dataset, datasetDef)
 		assert.NoError(t, err)
 
-		// check duration ms derived column
-		// assert.Equal(t, "GTE(INT($duration_ms), 50)", result.DurationMs.Name)
-		// assert.Equal(t, "derived_column", result.DurationMs.ColumnType)
+		// check Error field set to new column created
+		assert.Equal(t, "error_", result.Error.Name)
+		assert.Equal(t, "column", result.Error.ColumnType)
 
-		// check trace ID
+		// check trace ID unchanged
 		assert.Equal(t, "trace.trace_id", result.TraceID.Name)
 		assert.Equal(t, "column", result.TraceID.ColumnType)
 
-		// check Error unset field is still empty
-		assert.Equal(t, "", result.Error.Name)
-		assert.Equal(t, "", result.Error.ColumnType)
+		// check duration_ms update to null
+		// assert.Equal(t, "", result.DurationMs.Name)
+		// assert.Equal(t, "", result.DurationMs.ColumnType)
 	})
 
 	//Provider cleanup
@@ -65,4 +108,10 @@ func TestDatasetDefinitions(t *testing.T) {
 		err := c.DatasetDefinitions.Delete(ctx, dataset)
 		assert.NoError(t, err)
 	})
+
+	// t.Run("Get_notFound", func(t *testing.T) {
+	// 	_, err := c.DatasetDefinitions.Get(ctx, dataset)
+
+	// 	assert.Equal(t, ErrNotFound, err)
+	// })
 }
