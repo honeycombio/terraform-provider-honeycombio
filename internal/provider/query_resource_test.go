a3b686ee331f1aee0eaf92e4f855da725d8b74a1
Jason Harley <jason@honeycomb.io>
2024-03-18 15:03:30 -0400

    add update test to ensure query is replaced

--- internal/provider/query_resource_test.go
+++ internal/provider/query_resource_test.go
@@ -33,13 +33,24 @@ func TestAcc_QueryResource(t *testing.T) {
 		ProtoV5ProviderFactories: testAccProtoV5MuxServerFactory,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccConfigBasicQueryTest(dataset, col.KeyName),
+				Config: testAccConfigBasicQueryTest(dataset, col.KeyName, 1),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					testAccEnsureQueryExists(t, "honeycombio_query.test"),
 					resource.TestCheckResourceAttrSet("honeycombio_query.test", "id"),
 					resource.TestCheckResourceAttrSet("honeycombio_query.test", "query_json"),
 					resource.TestCheckResourceAttr("honeycombio_query.test", "dataset", dataset),
 				),
+			},
+			{
+				Config: testAccConfigBasicQueryTest(dataset, col.KeyName, 2),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					testAccEnsureQueryExists(t, "honeycombio_query.test"),
+					resource.TestCheckResourceAttrSet("honeycombio_query.test", "id"),
+					resource.TestCheckResourceAttrSet("honeycombio_query.test", "query_json"),
+					resource.TestCheckResourceAttr("honeycombio_query.test", "dataset", dataset),
+				),
+				PlanOnly:           true,
+				ExpectNonEmptyPlan: true,
 			},
 			{
 				ResourceName:        "honeycombio_query.test",
@@ -102,7 +113,7 @@ func TestAcc_QueryResourceUpgradeFromVersion022(t *testing.T) {
 	t.Cleanup(func() {
 		c.Columns.Delete(ctx, dataset, col.ID)
 	})
-	config := testAccConfigBasicQueryTest(dataset, col.KeyName)
+	config := testAccConfigBasicQueryTest(dataset, col.KeyName, 1)
 
 	resource.Test(t, resource.TestCase{
 		Steps: []resource.TestStep{
@@ -121,6 +132,8 @@ func TestAcc_QueryResourceUpgradeFromVersion022(t *testing.T) {
 			{
 				ProtoV5ProviderFactories: testAccProtoV5MuxServerFactory,
 				Config:                   config,
+				PlanOnly:                 true,
+				ExpectNonEmptyPlan:       false,
 			},
 		},
 	})
@@ -172,7 +185,7 @@ EOT
 	})
 }
 
-func testAccConfigBasicQueryTest(dataset, column string) string {
+func testAccConfigBasicQueryTest(dataset, column string, value float64) string {
 	return fmt.Sprintf(`
 data "honeycombio_query_specification" "test" {
   calculation {
@@ -183,14 +196,14 @@ data "honeycombio_query_specification" "test" {
   filter {
     column = "%[2]s"
     op     = ">"
-    value  = 1
+    value  = %[3]f
   }
 }
 
 resource "honeycombio_query" "test" {
   dataset    = "%[1]s"
   query_json = data.honeycombio_query_specification.test.json
-}`, dataset, column)
+}`, dataset, column, value)
 }
 
 func testAccEnsureQueryExists(t *testing.T, name string) resource.TestCheckFunc {
