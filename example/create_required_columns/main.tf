0075ebb6885764ba1d67f349412acd508805eaf4
Jason Harley <jason@honeycomb.io>
2024-04-30 18:58:39 -0400

    fix: update columns conditional creation example with an alternative

--- example/create_required_columns/main.tf
+++ example/create_required_columns/main.tf
@@ -1,5 +1,8 @@
 ####################################################
 # Example: Ensure required columns exist
+#
+# We call the Columns API directly to ensure that
+# the required columns exist.
 ####################################################
 
 variable "dataset" {
@@ -15,6 +18,7 @@ variable "honeycomb_api_endpoint" {
 #  See: https://docs.honeycomb.io/api/tag/Columns
 locals {
   required_columns = {
+    # name           type
     "db.system"   = "string",
     "db.type"     = "string",
     "duration_ms" = "float",
@@ -23,8 +27,22 @@ locals {
   }
 }
 
-# Call the Columns API to ensure that the required columns exist
+# Ensure the required columns exist. Example for Terraform 1.3 and below
 resource "null_resource" "ensure_columns" {
+  for_each = local.required_columns
+
+  provisioner "local-exec" {
+    command = <<-EOT
+curl -s ${var.honeycomb_api_endpoint}/1/columns/${var.dataset} \
+  -X POST \
+  -H "X-Honeycomb-Team: $${HONEYCOMB_API_KEY}" \
+  -d '{"key_name": "${each.key}", "type": "${each.value}"}'
+EOT
+  }
+}
+
+# The same as above but with the `terraform_data` resource for Terraform 1.4+
+resource "terraform_data" "ensure_columns" {
   for_each = local.required_columns
 
   provisioner "local-exec" {
