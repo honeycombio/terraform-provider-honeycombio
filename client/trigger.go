70c1bb1a65401e80560b326224d0fbb4490bdbf0
Jason Harley <jason@honeycomb.io>
2024-06-14 09:58:13 -0400

    still need that custom marshaler

--- client/trigger.go
+++ client/trigger.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 )
@@ -127,6 +128,33 @@ func TriggerThresholdOps() []TriggerThresholdOp {
 		TriggerThresholdOpLessThan,
 		TriggerThresholdOpLessThanOrEqual,
 	}
+}
+
+func (t *Trigger) MarshalJSON() ([]byte, error) {
+	// aliased type to avoid stack overflows due to recursion
+	type ATrigger Trigger
+
+	if t.QueryID != "" && t.Query != nil {
+		// we can't sent both to the API, so favour QueryID
+		// this doesn't work in the general case, but this
+		// client is now purpose-built for the Terraform provider
+		a := &ATrigger{
+			ID:                     t.ID,
+			Name:                   t.Name,
+			Description:            t.Description,
+			Disabled:               t.Disabled,
+			QueryID:                t.QueryID,
+			AlertType:              t.AlertType,
+			Threshold:              t.Threshold,
+			Frequency:              t.Frequency,
+			Recipients:             t.Recipients,
+			EvaluationScheduleType: t.EvaluationScheduleType,
+			EvaluationSchedule:     t.EvaluationSchedule,
+		}
+		return json.Marshal(&struct{ *ATrigger }{ATrigger: a})
+	}
+
+	return json.Marshal(&struct{ *ATrigger }{ATrigger: (*ATrigger)(t)})
 }
 
 func (s *triggers) List(ctx context.Context, dataset string) ([]Trigger, error) {
