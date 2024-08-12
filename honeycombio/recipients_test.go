f9d2c5f6bec2cb39d9f9f7df665df930473d6179
Jason Harley <jason@honeycomb.io>
2024-08-12 14:48:58 -0400

    adjust test case
    I didn't think through the "create with the old version" use case once the API was rejecting creation.
    It isn't really worth keeping an old one around for the purposes of this test: I did verify this logic
    here and manually though.

--- honeycombio/recipients_test.go
+++ honeycombio/recipients_test.go
@@ -41,38 +41,7 @@ resource "honeycombio_msteams_recipient" "test" {
   name = "test"
   url  = "https://example.com"
 }`,
-					ExpectError: regexp.MustCompile(`recipient creation is no longer allowed`),
-				},
-			},
-		})
-	})
-
-	t.Run("webhook recipient created with earlier version can be further managed", func(t *testing.T) {
-		config := func(name string) string {
-			return fmt.Sprintf(`
-resource "honeycombio_msteams_recipient" "test" {
-  name = "%s"
-  url  = "https://mycorp.example.net/webhooks/incoming/12345"
-}`, name)
-		}
-
-		resource.Test(t, resource.TestCase{
-			Steps: []resource.TestStep{
-				{
-					// create the recipient with v0.25.0 of the provider
-					ExternalProviders: map[string]resource.ExternalProvider{
-						"honeycombio": {
-							VersionConstraint: "0.25.0", // last version before the deprecation
-							Source:            "honeycombio/honeycombio",
-						},
-					},
-					Config: config(test.RandomStringWithPrefix("test.", 10)),
-				},
-				// update the recipient's name with the latest version of the provider
-				// and then allow the test to clean up the resource
-				{
-					ProtoV5ProviderFactories: testAccProtoV5ProviderFactory,
-					Config:                   config(test.RandomStringWithPrefix("test.", 10)),
+					ExpectError: regexp.MustCompile(`Creating new MSTeams recipients is no longer possible`),
 				},
 			},
 		})
