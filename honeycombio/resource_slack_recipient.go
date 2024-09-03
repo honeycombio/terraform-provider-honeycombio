11ea18ceaa5cfa406146cc37fb88860f8243cbbd
brookesargent <brookesargent@honeycomb.io>
2024-09-03 13:07:53 -0400

    update description too

--- honeycombio/resource_slack_recipient.go
+++ honeycombio/resource_slack_recipient.go
@@ -28,7 +28,7 @@ func newSlackRecipient() *schema.Resource {
 			"channel": {
 				Type:         schema.TypeString,
 				Required:     true,
-				Description:  "The Slack channel or username to send the notification to. Must begin with `#` or `@`.",
+				Description:  "The Slack channel or username to send the notification to. Must begin with `#` or `@` or be a valid channel id e.g. `CABC123DEF.",
 				ValidateFunc: validation.StringMatch(channelRegex, "channel must begin with `#` or `@` or be a valid channel id e.g. `CABC123DEF`"),
 			},
 		},
