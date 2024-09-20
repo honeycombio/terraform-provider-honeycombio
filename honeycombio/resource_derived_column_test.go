1b4a5f6ae629fcde07b0d0bdf083fa5c579354c1
Jason Harley <jason@honeycomb.io>
2024-09-20 09:16:54 -0400

    fix(r/derived_column): tidy up test (#547)
    Noticed this test was using a fixed name during another investigation.
    
    Tidies up the test a bit as well.

--- honeycombio/resource_derived_column_test.go
+++ honeycombio/resource_derived_column_test.go
@@ -6,81 +6,56 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/honeycombio/terraform-provider-honeycombio/internal/helper/test"
 )
 
 func TestAccHoneycombioDerivedColumn_basic(t *testing.T) {
 	dataset := testAccDataset()
+	alias := test.RandomStringWithPrefix("test.", 10)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 testAccPreCheck(t),
 		ProtoV5ProviderFactories: testAccProtoV5ProviderFactory,
-		IDRefreshName:            "honeycombio_derived_column.test",
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDerivedColumnConfig(dataset, "duration_ms_log10"),
+				Config: fmt.Sprintf(`
+resource "honeycombio_derived_column" "test" {
+  alias       = "%s"
+  expression  = "BOOL(1)"
+  description = "my test description"
+
+  dataset = "%s"
+}`, alias, dataset),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "alias", "duration_ms_log10"),
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "expression", "LOG10($duration_ms)"),
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "description", "LOG10 of duration_ms"),
+					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "alias", alias),
+					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "expression", "BOOL(1)"),
+					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "description", "my test description"),
 				),
 			},
 			{
 				ResourceName:      "honeycombio_derived_column.test",
-				ImportStateId:     fmt.Sprintf("%s/%s", dataset, "duration_ms_log10"),
+				ImportStateId:     fmt.Sprintf("%s/%s", dataset, alias),
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 		},
 	})
 
-	// validate 'pretty' alias'
 	resource.Test(t, resource.TestCase{
 		PreCheck:                 testAccPreCheck(t),
 		ProtoV5ProviderFactories: testAccProtoV5ProviderFactory,
-		IDRefreshName:            "honeycombio_derived_column.test",
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDerivedColumnConfig(dataset, "LOG(10) duration_ms"),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "alias", "LOG(10) duration_ms"),
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "expression", "LOG10($duration_ms)"),
-					resource.TestCheckResourceAttr("honeycombio_derived_column.test", "description", "LOG10 of duration_ms"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccHoneycombioDerivedColumn_error(t *testing.T) {
-	dataset := testAccDataset()
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 testAccPreCheck(t),
-		ProtoV5ProviderFactories: testAccProtoV5ProviderFactory,
-		IDRefreshName:            "honeycombio_derived_column.test",
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
 resource "honeycombio_derived_column" "invalid_column_in_expression" {
-  alias       = "invalid_column_in_expression"
+  alias       = "%s"
   expression  = "LOG10($invalid_column)"
 
   dataset = "%s"
-}
-`, dataset),
+}`, alias, dataset),
 				ExpectError: regexp.MustCompile(`Error: unknown column name: invalid_column`),
 			},
 		},
 	})
-}
-
-func testAccDerivedColumnConfig(dataset, alias string) string {
-	return fmt.Sprintf(`
-resource "honeycombio_derived_column" "test" {
-  alias       = "%s"
-  expression  = "LOG10($duration_ms)"
-  description = "LOG10 of duration_ms"
-
-  dataset = "%s"
-}`, alias, dataset)
 }
