d58baeff5233f20ccee7653fe02b3bc3a7c582c0
Jason Harley <jason@honeycomb.io>
2024-11-15 09:40:33 -0500

    fix(client): retry on server errors (#571)
    Have the V1 API Client retry on 500-class errors like the V2 client.

--- client/client.go
+++ client/client.go
@@ -237,7 +237,7 @@ func (c *Client) newRequest(ctx context.Context, method, path string, body any) 
 }
 
 // retryHTTPCheck provides a callback for Client.CheckRetry which
-// will retry both rate limit (429) and server gateway (502, 504) errors.
+// will retry both rate limit (429) and server (5xx) errors.
 func (c *Client) retryHTTPCheck(ctx context.Context, resp *http.Response, err error) (bool, error) {
 	if ctx.Err() != nil {
 		return false, ctx.Err()
@@ -247,9 +247,7 @@ func (c *Client) retryHTTPCheck(ctx context.Context, resp *http.Response, err er
 	}
 
 	if resp != nil {
-		if resp.StatusCode == http.StatusTooManyRequests ||
-			resp.StatusCode == http.StatusBadGateway ||
-			resp.StatusCode == http.StatusGatewayTimeout {
+		if resp.StatusCode == http.StatusTooManyRequests || resp.StatusCode >= 500 {
 			return true, nil
 		}
 	}
