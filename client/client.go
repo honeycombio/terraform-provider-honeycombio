bd2a59efdeb5129a981b47afa3002a156259af56
Elliott Krupa <elliottkrupa@honeycomb.io>
2024-11-20 18:05:07 -0500

    update v2 client

--- client/client.go
+++ client/client.go
@@ -138,10 +138,9 @@ func NewClientWithConfig(config *Config) (*Client, error) {
 	}
 
 	if config.Debug {
-		// if enabled we log all requests and responses to sterr
 		client.httpClient.Logger = log.New(os.Stderr, "", log.LstdFlags)
 		client.httpClient.ResponseLogHook = func(l retryablehttp.Logger, resp *http.Response) {
-			l.Printf("[DEBUG] Request: %s \"%s\" %s %s", resp.Request.Method, resp.Request.URL.String(), resp.Status, resp.Header.Get("Request-Id"))
+			l.Printf("[DEBUG] %s \"%s\" %s %s", resp.Request.Method, resp.Request.URL.String(), resp.Status, resp.Header.Get("Request-Id"))
 		}
 	}
 
