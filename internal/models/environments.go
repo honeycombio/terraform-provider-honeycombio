a6cd547ff45961b7b03a1335cf80f47566a75bcb
Jason Harley <jason@honeycomb.io>
2025-04-14 09:20:37 -0400

    feat(d/environment): add detail_filter to simplify environment lookup

--- internal/models/environments.go
+++ internal/models/environments.go
@@ -5,12 +5,13 @@ import (
 )
 
 type EnvironmentResourceModel struct {
-	ID              types.String `tfsdk:"id"`
-	Name            types.String `tfsdk:"name"`
-	Slug            types.String `tfsdk:"slug"`
-	Description     types.String `tfsdk:"description"`
-	Color           types.String `tfsdk:"color"`
-	DeleteProtected types.Bool   `tfsdk:"delete_protected"`
+	ID              types.String        `tfsdk:"id"`
+	DetailFilter    []DetailFilterModel `tfsdk:"detail_filter"`
+	Name            types.String        `tfsdk:"name"`
+	Slug            types.String        `tfsdk:"slug"`
+	Description     types.String        `tfsdk:"description"`
+	Color           types.String        `tfsdk:"color"`
+	DeleteProtected types.Bool          `tfsdk:"delete_protected"`
 }
 
 type EnvironmentsDataSourceModel struct {
--- internal/provider/environment_data_source_test.go
+++ internal/provider/environment_data_source_test.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -19,19 +20,62 @@ func TestAcc_EnvironmentDataSource(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
-data "honeycombio_environment" "test" {
+data "honeycombio_environment" "test_id" {
   id = "%s"
-}`, env.ID),
+}
+
+data "honeycombio_environment" "test_filter" {
+  detail_filter {
+    name = "name"
+    value = "%s"
+  }
+}`, env.ID, env.Name),
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "id", env.ID),
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "name", env.Name),
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "slug", env.Slug),
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "color", *env.Color),
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "description", *env.Description),
-					resource.TestCheckResourceAttr("data.honeycombio_environment.test", "delete_protected", "true"),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "id", env.ID),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "name", env.Name),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "slug", env.Slug),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "color", *env.Color),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "description", *env.Description),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_id", "delete_protected", "true"),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "id", env.ID),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "name", env.Name),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "slug", env.Slug),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "color", *env.Color),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "description", *env.Description),
+					resource.TestCheckResourceAttr("data.honeycombio_environment.test_filter", "delete_protected", "true"),
 				),
 			},
 		},
 	})
 
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 testAccPreCheckV2API(t),
+		ProtoV5ProviderFactories: testAccProtoV5ProviderFactory,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+data "honeycombio_environment" "test" {
+  id = "boop"
+
+  detail_filter {
+    name  = "name"
+    value = "boop"
+  }
+}`,
+				ExpectError: regexp.MustCompile(`Attribute "detail_filter" cannot be specified when "id" is specified`),
+				PlanOnly:    true,
+			},
+			{
+				Config: `
+data "honeycombio_environment" "test" {
+  detail_filter {
+    name  = "name"
+    value = "boop"
+  }
+}`,
+				ExpectError: regexp.MustCompile(`Your filter returned no matches`),
+				PlanOnly:    true,
+			},
+		},
+	})
 }
