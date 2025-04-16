fc4241ea870eb6c88f24de3bdebc6da1820531d2
Jason Harley <jason@honeycomb.io>
2025-04-16 15:31:18 -0400

    feat(d/environment): add detail_filter to simplify environment lookup (#660)
    Received feedback that looking up environment IDs was a bit awkward.
    
    This allows looking up a single environment via a filter: the "no match"
    behaviour may feel like a breaking change but when `id` was the only
    option the data source would fail with 404 anyway, so, the same
    net-result.

--- internal/models/environments.go
+++ internal/models/environments.go
@@ -13,6 +13,16 @@ type EnvironmentResourceModel struct {
 	DeleteProtected types.Bool   `tfsdk:"delete_protected"`
 }
 
+type EnvironmentDataSourceModel struct {
+	ID              types.String        `tfsdk:"id"`
+	DetailFilter    []DetailFilterModel `tfsdk:"detail_filter"`
+	Name            types.String        `tfsdk:"name"`
+	Slug            types.String        `tfsdk:"slug"`
+	Description     types.String        `tfsdk:"description"`
+	Color           types.String        `tfsdk:"color"`
+	DeleteProtected types.Bool          `tfsdk:"delete_protected"`
+}
+
 type EnvironmentsDataSourceModel struct {
 	ID           types.String        `tfsdk:"id"`
 	DetailFilter []DetailFilterModel `tfsdk:"detail_filter"`
