30e28f8ce651673cc5d2651ae0cfceb37396eba3
Ronald <ronaldekambi@honeycomb.io>
2025-05-09 14:55:33 -0400

    feat(boards): Support panels in board client (#676)
    ## Which problem is this PR solving?
    
    - Breaking down my massive PR into incremental PRs
    - This adds panels to board client for flexible boards support

--- client/board_test.go
+++ client/board_test.go
@@ -143,3 +143,109 @@ func TestClassicBoards(t *testing.T) {
 		assert.True(t, de.IsNotFound())
 	})
 }
+
+func TestFlexibleBoards(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	var flexibleBoard *client.Board
+	var err error
+
+	c := newTestClient(t)
+	dataset := testDataset(t)
+
+	column, err := c.Columns.Create(ctx, dataset, &client.Column{
+		KeyName: test.RandomStringWithPrefix("test.", 8),
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		c.Columns.Delete(ctx, dataset, column.ID)
+	})
+
+	query, err := c.Queries.Create(ctx, dataset, &client.QuerySpec{
+		Calculations: []client.CalculationSpec{
+			{
+				Op:     client.CalculationOpAvg,
+				Column: &column.KeyName,
+			},
+		},
+		TimeRange: client.ToPtr(3600), // 1 hour
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, query.ID)
+
+	qa := &client.QueryAnnotation{
+		Name:        test.RandomStringWithPrefix("test.", 20),
+		Description: "This derived column is created by a test",
+		QueryID:     *query.ID,
+	}
+	queryAnnotation, err := c.QueryAnnotations.Create(ctx, dataset, qa)
+	require.NoError(t, err)
+	require.NotNil(t, queryAnnotation.ID)
+
+	sli, err := c.DerivedColumns.Create(ctx, dataset, &client.DerivedColumn{
+		Alias:      test.RandomStringWithPrefix("test.", 8),
+		Expression: "BOOL(1)",
+	})
+	require.NoError(t, err)
+	slo, err := c.SLOs.Create(ctx, dataset, &client.SLO{
+		Name:             test.RandomStringWithPrefix("test.", 8),
+		TimePeriodDays:   7,
+		TargetPerMillion: 990000,
+		SLI:              client.SLIRef{Alias: sli.Alias},
+	})
+	require.NoError(t, err)
+	t.Cleanup(func() {
+		c.SLOs.Delete(ctx, dataset, slo.ID)
+		c.DerivedColumns.Delete(ctx, dataset, sli.ID)
+	})
+
+	t.Run("Create flexible board", func(t *testing.T) {
+		data := &client.Board{
+			Name:        test.RandomStringWithPrefix("test.", 8),
+			BoardType:   "flexible",
+			Description: "A board with some panels",
+			Panels: []client.BoardPanel{
+				{
+					PanelType: client.BoardPanelTypeSLO,
+					PanelPosition: client.BoardPanelPosition{
+						X:      6,
+						Y:      0,
+						Height: 3,
+						Width:  4,
+					},
+					SLOPanel: &client.BoardSLOPanel{
+						SLOID: slo.ID,
+					},
+				},
+			},
+		}
+		flexibleBoard, err = c.Boards.Create(ctx, data)
+		require.NoError(t, err)
+		assert.NotNil(t, flexibleBoard.ID)
+
+		// copy ID before asserting equality
+		data.ID = flexibleBoard.ID
+
+		// ensure the board URL got populated
+		assert.NotEmpty(t, flexibleBoard.Links.BoardURL)
+		data.Links.BoardURL = flexibleBoard.Links.BoardURL
+
+		assert.Equal(t, data, flexibleBoard)
+	})
+
+	t.Run("List", func(t *testing.T) {
+		result, err := c.Boards.List(ctx)
+		require.NoError(t, err)
+
+		assert.Contains(t, result, *flexibleBoard, "could not find newly created board with List")
+	})
+
+	t.Run("Get", func(t *testing.T) {
+		board, err := c.Boards.Get(ctx, flexibleBoard.ID)
+		require.NoError(t, err)
+
+		assert.Equal(t, *board, *flexibleBoard)
+	})
+
+}
