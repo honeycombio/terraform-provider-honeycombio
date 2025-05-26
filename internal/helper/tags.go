4cb05d38db3cf810f3c7d30101c8606e3e99b000
Elim Tsiagbey <elimtsiagbey@honeycomb.io>
2025-05-26 16:50:38 -0400

    lint fix

--- internal/helper/tags.go
+++ internal/helper/tags.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
 	"github.com/honeycombio/terraform-provider-honeycombio/client"
 )
 
