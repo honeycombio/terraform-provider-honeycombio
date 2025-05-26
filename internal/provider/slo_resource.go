2d326370ef91eed145e5cd014b4c409f9a23aa01
Elim Tsiagbey <elimtsiagbey@honeycomb.io>
2025-05-26 11:35:20 -0400

    fix lint

--- internal/provider/slo_resource.go
+++ internal/provider/slo_resource.go
@@ -18,6 +18,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/setplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
 	"github.com/honeycombio/terraform-provider-honeycombio/client"
 	"github.com/honeycombio/terraform-provider-honeycombio/internal/helper"
 	"github.com/honeycombio/terraform-provider-honeycombio/internal/helper/modifiers"
